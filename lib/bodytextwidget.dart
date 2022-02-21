6712c19bc98d20401f275b63a11b14e851d42ae5
Emmanuel Sanchez <fede@fede.website>
2022-02-21 15:23:25 -0600

    the body is on a widget

---
+++ lib/bodytextwidget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class bodytextwidget extends StatefulWidget {
+  bodytextwidget({Key? key}) : super(key: key);
+
+  @override
+  State<bodytextwidget> createState() => _bodytextwidgetState();
+}
+
+class _bodytextwidgetState extends State<bodytextwidget> {
+  int count = 0;
+
+  String bodyText = 'Text of the body part version:';
+
+  void add() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text('$bodyText $count'),
+          ElevatedButton(
+              child: Text('$bodyText $count'),
+              onPressed: () {
+                add();
+              })
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import './appbarwid.dart';
+import './bodytextwidget.dart';
 
 void main(List<String> args) {
   runApp(basics());
@@ -10,44 +12,10 @@ class basics extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Time to practice: Flutter Basics"),
+          title: Text('hello'),
         ),
         body: bodytextwidget(),
-      ),
-    );
-  }
-}
-
-class bodytextwidget extends StatefulWidget {
-  bodytextwidget({Key? key}) : super(key: key);
-
-  @override
-  State<bodytextwidget> createState() => _bodytextwidgetState();
-}
-
-class _bodytextwidgetState extends State<bodytextwidget> {
-  int count = 0;
-
-  String bodyText = 'Text of the body part version:';
-
-  void add() {
-    setState(() {
-      count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text('$bodyText is $count'),
-          ElevatedButton(
-              child: Text('$bodyText is $count'),
-              onPressed: () {
-                add();
-              })
-        ],
+        // body: bodytextwidget(),
       ),
     );
   }
