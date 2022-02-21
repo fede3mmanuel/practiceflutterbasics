0975dd49b173ca4dcca82b955915364038ea367d
Emmanuel Sanchez <fede@fede.website>
2022-02-21 12:22:14 -0600

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main(List<String> args) {
+  runApp(basics());
+}
+
+class basics extends StatelessWidget {
+  const basics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(child: Text('Hello world')),
+    );
+  }
+}
